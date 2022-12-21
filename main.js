162b803364a4515e3ea9a61754a0ffa1ac6e2a3d
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2022-12-21 18:02:07 +0100

    reworking function to be able to add and delete todo

--- main.js
+++ main.js
@@ -5,14 +5,5 @@ function main() {
 }
 
 function addTodo() {
-    const addBtn = document.getElementById('addTodo');
-    const tasks = document.getElementById('tasks');
-    const inputField = document.getElementById('inputField');
 
-    addBtn.addEventListener('click', () => {
-        const paragraph = document.createElement('p');
-        paragraph.innerText = inputField.value;
-        paragraph.classList.add('p')
-        tasks.appendChild(paragraph);
-    });
-}
+  
