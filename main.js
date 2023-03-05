9700ca4e2a828f5679ce80dabf0e7c0039cf139f
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-05 20:03:48 +0100

    finish todolist with add-delete todos with LS

--- main.js
+++ main.js
@@ -1,48 +1,115 @@
-window.addEventListener('DOMContentLoaded', main);
+const todoList = document.getElementById("todoList");
+const newTodoInput = document.getElementById("newTodoInput");
+const addTodoButton = document.getElementById("addTodoButton");
 
-function main() {
-  const form = document.getElementById("todo-form");
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
+let todos = [];
 
-    addTodo();
-  });
+// Load saved todos from local storage
+function loadTodos() {
+  const savedTodos = localStorage.getItem("todos");
+  if (savedTodos) {
+    todos = JSON.parse(savedTodos);
+  }
 }
 
+// Save LocalStorage
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
 
+// Add a new todo
 function addTodo() {
-  const todoInput = document.getElementById("todo-input").value;
-  const newLi = document.createElement("li");
-  newLi.classList.add('todo-item')
-  newLi.textContent = todoInput;
-
-  const divTasks = document.createElement("div");
-  divTasks.classList.add("divTasks")
-
-  divTasks.append(newLi)
-
-   //EDIT BTN 
-   const editBtn = document.createElement("button");
-   editBtn.textContent = "Edit";
-   editBtn.classList.add("edit-btn");
-   editBtn.addEventListener("click", editTodo);
-   divTasks.append(editBtn);
-  
-  // DELETE BTN
-  const removeBtn = document.createElement("button");
-  removeBtn.textContent = "Remove";
-  removeBtn.classList.add("remove-btn");
-  removeBtn.addEventListener("click", removeTodo);
-  divTasks.append(removeBtn);
-
-  const todoList = document.getElementById("todo-list");
-  todoList.appendChild(divTasks);
-
-  document.getElementById("todo-input").value = "";
+  const todoText = newTodoInput.value.trim();
+  if (!todoText) {
+    return;
+  }
+  const todo = { text: todoText, done: false };
+  todos.push(todo);
+  saveTodos();
+  renderTodos();
+  newTodoInput.value = "";
 }
 
-
-// Remove todo
-function removeTodo() {
-  this.parentNode.remove();
+// Delete or edit a todo
+function deleteOrEditTodo(event) {
+  const target = event.target;
+  if (target.type === "checkbox") {
+    const li = target.parentNode;
+    const index = Array.prototype.indexOf.call(todoList.children, li);
+    todos[index].done = target.checked;
+    saveTodos();
+    li.classList.toggle("done");
+  } else if (target.tagName === "BUTTON") {
+    const li = target.parentNode;
+    const index = Array.prototype.indexOf.call(todoList.children, li);
+    todos.splice(index, 1);
+    saveTodos();
+    li.remove();
+  } else if (target.tagName === "SPAN") {
+    const span = target;
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = span.innerText;
+    input.addEventListener("blur", function () {
+      const newTodoText = input.value.trim();
+      if (newTodoText) {
+        const li = input.parentNode;
+        const index = Array.prototype.indexOf.call(todoList.children, li);
+        todos[index].text = newTodoText;
+        saveTodos();
+        span.innerText = newTodoText;
+      } else {
+        const li = input.parentNode;
+        const index = Array.prototype.indexOf.call(todoList.children, li);
+        input.value = todos[index].text;
+      }
+      span.style.display = "inline";
+      input.remove();
+    });
+    span.style.display = "none";
+    span.parentNode.insertBefore(input, span);
+    input.focus();
+  }
 }
+
+function renderTodos() {
+  todoList.innerHTML = "";
+  for (let i = 0; i < todos.length; i++) {
+    const todo = todos[i];
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.done;
+    checkbox.addEventListener("change", deleteOrEditTodo);
+    const span = document.createElement("span");
+    span.innerText = todo.text;
+    span.addEventListener("dblclick", deleteOrEditTodo);
+    const button = document.createElement("button");
+    button.innerText = "Delete";
+    button.addEventListener("click", deleteOrEditTodo);
+    li.append(checkbox);
+    li.append(span);
+    li.append(button);
+    if (todo.done) {
+      li.classList.add("done");
+    }
+    todoList.append(li);
+  }
+}
+
+// Load saved todos and render them
+loadTodos();
+renderTodos();
+
+// Add event listeners
+addTodoButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  addTodo();
+});
+
+newTodoInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addTodo();
+  }
+});
